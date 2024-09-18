71743c956e4867274bf1cb4d240a7e93fd54df04
Khushi Johri <khushijohri2001@gmail.com>
2024-09-18 23:43:17 +0530

    Result card displayed

--- quiz.js
+++ quiz.js
@@ -3,79 +3,94 @@ import { questionList } from "./utils/questionList.js";
 const question = document.querySelector(".question");
 const options = document.querySelector(".options");
 const countDownValue = document.querySelector(".countDownValue");
+const quizCard = document.querySelector(".card");
+const quizCardElements = document.querySelectorAll(".card div");
 
-let currentQuestion = 0;
+let currentQuestionIndex = 0;
 let score = 0;
 
-
 const createOption = (optionText) => {
-    const createElement = document.createElement("button");
-    const createTextNode = document.createTextNode(optionText);
+  const createElement = document.createElement("button");
+  const createTextNode = document.createTextNode(optionText);
 
-    createElement.appendChild(createTextNode);
-    createElement.classList.add("option");
+  createElement.appendChild(createTextNode);
+  createElement.classList.add("option");
 
-    options.appendChild(createElement);
+  options.appendChild(createElement);
 };
 
 const deleteOption = () => {
-    document.querySelector(".option").remove();
+  document.querySelector(".option").remove();
 };
 
 const countDown = () => {
-    let initialCountDownValue = 5;
+  let initialCountDownValue = 5;
 
-    const timer = setInterval(() => {
-        countDownValue.innerText = initialCountDownValue;
-        initialCountDownValue--;
+  const timer = setInterval(() => {
+    countDownValue.innerText = initialCountDownValue;
+    initialCountDownValue--;
 
-        if (initialCountDownValue < 0) {
-            clearInterval(timer);
-            questionList[currentQuestion].options.forEach(() => deleteOption());
-            currentQuestion = currentQuestion + 1;
-            countDownValue.innerText = 5;
+    if (initialCountDownValue < 0) {
+      clearInterval(timer);
+      questionList[currentQuestionIndex].options.forEach(() => deleteOption());
+      currentQuestionIndex = currentQuestionIndex + 1;
+      countDownValue.innerText = 5;
 
-            if (currentQuestion + 1 <= questionList.length) {
-                startQuiz();
-            }
-        }
-    }, 1000);
+      if (currentQuestionIndex + 1 <= questionList.length) {
+        startQuiz();
+      } else {
+        quizCardElements.forEach((element) =>
+          element.classList.add("display-hide")
+        );
+
+        displayResult();
+      }
+    }
+  }, 1000);
 };
 
+const displayResult = () => {
+  const createHeading = document.createElement("h3");
+  createHeading.innerText = `Your score is: ${score}/${questionList.length}`;
+  quizCard.append(createHeading);
+};
 
 const startQuiz = () => {
+  if (currentQuestionIndex < questionList.length) {
+    countDown();
+  }
+  question.innerText = `Q ${currentQuestionIndex + 1} ${
+    questionList[currentQuestionIndex].question
+  }`;
+  questionList[currentQuestionIndex].options.forEach((option) => {
+    createOption(option);
+  });
 
-    if (currentQuestion < questionList.length) {
-        countDown();
-    }
-    question.innerText = `Q ${currentQuestion + 1} ${questionList[currentQuestion].question}`;
-    questionList[currentQuestion].options.forEach((option) => {
-        createOption(option);
+  const optionParent = document.querySelector(".options");
+
+  optionParent.addEventListener("click", (e) => {
+    const optionList = document.querySelectorAll("button");
+
+    optionList.forEach((option) => {
+      const selectedOption = e.target;
+
+      if (
+        selectedOption.nodeName === "BUTTON" &&
+        selectedOption.innerText === option.innerText
+      ) {
+        if (
+          selectedOption.innerText === questionList[currentQuestionIndex].correct
+        ) {
+          selectedOption.classList.add("correct-option");
+
+          console.log(selectedOption);
+        } else {
+          selectedOption.classList.add("incorrect-option");
+        }
+      }
+      option.disabled = true;
     });
-
-    const optionParent = document.querySelector(".options");
-
-    optionParent.addEventListener("click", (e) => {
-        const optionList = document.querySelectorAll("button");
-
-        optionList.forEach(option => {
-            const selectedOption = e.target;
-
-            if (selectedOption.nodeName === "BUTTON" && selectedOption.innerText === option.innerText) {
-                if (selectedOption.innerText === questionList[currentQuestion].correct) {
-                    selectedOption.classList.add("correct-option");
-                    console.log(selectedOption);
-                    
-
-                } else {
-                    selectedOption.classList.add("incorrect-option")
-                }
-            }
-            option.disabled = true
-        });
-    })
-}
-
-
+  });
+};
 
 startQuiz();
